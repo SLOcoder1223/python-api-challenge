b2ac68fb8804088e56499db48e75057309ac8ce8
Dane Smith <danesmith@Danes-MBP.lan>
2023-06-12 14:12:23 -0700

    homework done

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "87805d9cc07a40c74d63a3658926cd7f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "bNwZy-iJ90jy915yZREZz3Gp3bUGlguu"
